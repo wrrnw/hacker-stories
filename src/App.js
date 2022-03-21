116151a487d2409bfe47d5b9ef570374e2f434fb
Warren Wei <warrenwei.work@gmail.com>
2022-03-21 18:26:19 +1100

    Learn pass data from child to parent

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    }
+  ];
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  // A
+  const handleSearch = (event) => {
+    // C
+    setSearchTerm(event.target.value);
+  }
+
+  const searchedStories = stories.filter(story => (
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  ));
+
+  console.log("App Render");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hacker Stories</h1>
+      {/* //B */}
+      <Search search={searchTerm} onSearch={handleSearch}/>
+      <p>Search Item {searchTerm}</p>
+      <hr /> 
+      <List list={searchedStories}/>
     </div>
   );
-}
+};
+
+const Search = (props) => {
+  console.log("Search Render");
+
+  return (
+    <div>
+      <lable htmlFor="search">Search: </lable>
+      <input id="search" type="text" value={props.search} onChange={props.onSearch}/>
+    </div>
+  );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
+    ))}
+  </ul>
+);
+
+const Item = (props) => (
+  <li key={props.item.objectID}>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
 
 export default App;
